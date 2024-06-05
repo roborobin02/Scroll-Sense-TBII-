6c04cb73cd6834f53bee650eee249c5414759914
roborobin02 <robinpaul4902@gmail.com>
2024-06-05 16:23:01 +0200

    Add files via upload

--- ScrollSensev3.py
+++ ScrollSensev3.py
@@ -45,7 +45,23 @@ def create_startpage(root, image_file_path):
     It places a background image and places a button at the bottom"""
 
     # place a background image on the home page using the set background definition
-    set_background(root, image_file_path) #="/Users/robinpaul/Desktop/TechBasics2/images/homepage.jpg")
+    set_background(root, image_file_path) #="/Users/robinpaul/Desktop/TechBasics2/images/ScrollSenseScreenshot1.jpg")
+
+
+    intro_frame = tk.Frame(root)
+    intro_frame.pack(side="top", fill="both", expand=True)
+
+    intro_label = tk.Label(intro_frame, text="Welcome to the ScrollSense Prototype!\nPlease enter your name:")
+    intro_label.place(x=270, y=450, anchor="center")
+
+    name_entry = tk.Entry(intro_frame)
+    name_entry.place(x=270, y=500, anchor="center")
+
+    daily_time_label = tk.Label(intro_frame, text="Please tell me your daily time on Instagram (in Hours):")
+    daily_time_label.place(x=270, y=550, anchor="center")
+
+    daily_time_entry = tk.Entry(intro_frame)
+    daily_time_entry.place(x=270, y=600, anchor="center")
 
     # place a button that will go a new page
     newpage_button = tk.Button(root,
@@ -56,7 +72,7 @@ def create_startpage(root, image_file_path):
     newpage_button.pack(side="bottom")
 
 
-image_file_path = 'images/homepage.jpg'
+image_file_path = 'images/ScrollSenseScreenshot1.jpg'
 create_startpage(root, image_file_path)
 
 root.mainloop()
